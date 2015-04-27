3c9047e7d29023b3f5baaf3c75e2500aa3c978d6
Bharath <bharathh4@gmail.com>
2015-04-26 20:53:04 -0500

    Removed comments and realigned code and added the avsr directory to matlab path

--- main.m
+++ main.m
@@ -2,7 +2,6 @@ clc
 close all
 clear all
 
-
 format long
 
 % Traiin with one speaker
@@ -43,7 +42,6 @@ wavfile='amdc.wav'
 videofile='amdc.mp4'
 train_hmm(speaker_directory,word,wavfile,videofile)
 
-
 % Traiin with fifth speaker
 restoredefaultpath
 cd 'C:\Users\TheatroIT\Documents\MSP\Matlab\AudioVisualCrisp'
@@ -52,9 +50,6 @@ word='richardson'
 wavfile='amdc.wav'
 videofile='amdc.mp4'
 train_hmm(speaker_directory,word,wavfile,videofile)
-
-
-
 
 % Traiin with sixth speaker
 restoredefaultpath
@@ -65,9 +60,6 @@ wavfile='amdc.wav'
 videofile='amdc.mp4'
 train_hmm(speaker_directory,word,wavfile,videofile)
 
-
-
-
 % Traiin with seventh speaker
 restoredefaultpath
 cd 'C:\Users\TheatroIT\Documents\MSP\Matlab\AudioVisualCrisp'
@@ -76,9 +68,6 @@ word='bluethinman'
 wavfile='amdc.wav'
 videofile='amdc.mp4'
 train_hmm(speaker_directory,word,wavfile,videofile)
-
-
-
 
 AL=[]
 VL=[]
@@ -92,67 +81,29 @@ first_speaker_index=1
 
 % there is a problem with i=1
 for j=first_speaker_index:7
-   
-     AL=[];
-     VL=[];
-
+    
+    AL=[];
+    VL=[];
+    
     % there is a problem with i=1
     for i=first_speaker_index:7
-
+        
+        
+        addpath('C:\Users\TheatroIT\Documents\MSP\Matlab\AudioVisualCrisp');
+        
         %% Load trained hmm of each speaker. Small change in comment
-%         all_speaker_directory='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\train\'
-%         audio_hmm_filename='\word_audio_hmm.mat';
-%         video_hmm_filename='\word_video_hmm.mat';
-% 
-%         src=sprintf('%ss%d%s',all_speaker_directory,i,audio_hmm_filename)
-%         destination='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
-%         copyfile(src,destination);
-%         src=sprintf('%ss%d%s',all_speaker_directory,i,video_hmm_filename)
-%         destination='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
-%         copyfile(src,destination);
-
-        % Replaced the above commented section callled "Load trained hmm of each speaker"
+        
         src='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\train\';
         destination='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
         copy_trained_hmms(src,destination,i);
         
         
         %% Load testing data of a speaker
-%         wav_filename='\amdc.wav';
-%         transcription_filename='\transcription.txt';
-%        
-%         % Copy  speaker data with each trained speaker
-%         restoredefaultpath
-%         cd 'C:\Users\TheatroIT\Documents\MSP\Matlab\AudioVisualCrisp'
-%         
-%         test_speaker_directory='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\test\'
-%         workbench_directory='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
-%         features_filename='\features.mat'
-%         transcription_filename='\transcription.txt';
-%         
-%         src=sprintf('%ss%d%s',test_speaker_directory,j,wav_filename);
-%         destination='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
-%         copyfile(src,destination);
-%         
-%         src=sprintf('%ss%d%s',test_speaker_directory,j,transcription_filename);
-%         destination='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
-%         copyfile(src,destination);
-% 
-%         
-%         videofile='amdc.mp4'
-%         videoFolder=sprintf('%ss%d\\',test_speaker_directory,j);
-%         features_word=visual_features_manager(videoFolder,videofile);
-% 
-%         
-%         src=sprintf('%ss%d%s',test_speaker_directory,j,features_filename);
-%         destination='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
-%         copyfile(src,destination); 
-
-            % Achives the above code block via a function             
-
-            src='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\test\';
-            destination='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
-            copy_test_speaker_data(src,destination,j)
+        
+        
+        src='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\test\';
+        destination='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\work_board\';
+        copy_test_speaker_data(src,destination,j)
         
         % Test with speakers
         restoredefaultpath
@@ -162,40 +113,29 @@ for j=first_speaker_index:7
         wavfile='amdc.wav'
         videofile='amdc.mp4'
         [al,vl]=test_hmm(destination,word,wavfile,videofile)
-       
-
+        
+        
         cd 'C:\Users\TheatroIT\Documents\MSP\Matlab\AudioVisualCrisp'
-
+        
         AL=[AL;al]
         VL=[VL;vl]
-
+        
     end
-
-    [val index]=max(AL); 
+    
+    [val index]=max(AL);
     display(sprintf('The audio hmm says  %d',index));
     prediction_audio=[prediction_audio;index+(first_speaker_index-1)];
-    [val index]=max(VL); 
+    [val index]=max(VL);
     display(sprintf('The video hmm says  %d',index));
     prediction_video=[prediction_video;index+(first_speaker_index-1)];
-
-    [val index]=max(normalize(AL)+normalize(VL)); 
+    
+    [val index]=max(normalize(AL)+normalize(VL));
     display(sprintf('The audio-video combined hmm says  %d',index));
     prediction_audiovideo=[prediction_audiovideo;index+(first_speaker_index-1)];
-
+    
     ground_truth=[ground_truth;j];
 end
 
 results=[prediction_audio prediction_video prediction_audiovideo ground_truth]
 csvwrite('results.csv',results)
-
-
-% % Test with anothera
-% restoredefaultpath
-% cd 'C:\Users\TheatroIT\Documents\MSP\Matlab\AudioVisualCrisp'
-% speaker_directory='C:\Users\TheatroIT\Documents\MSP\Matlab\Speakers\test\s2\'
-% word='chennai'
-% test_hmm(speaker_directory,word)
-% cd 'C:\Users\TheatroIT\Documents\MSP\Matlab\AudioVisualCrisp'
-
-
 
